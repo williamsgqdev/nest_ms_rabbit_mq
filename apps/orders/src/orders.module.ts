9897e387722e256240e3a0de0fe9bec14e658271
williamsgqdev <williamsagbunu@gmail.com>
2023-10-24 00:51:37 +0100

    common library

---
+++ apps/orders/src/orders.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  controllers: [OrdersController],
+  providers: [OrdersService],
+})
+export class OrdersModule {}
